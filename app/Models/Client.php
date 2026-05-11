494fa8ae4969216f4042c467d00e8f3d110b3c19
Omar Abi Farraj <omar.7tech@gmail.com>
2026-05-12 00:15:01 +0300

    edit the clieant mnodel and migration

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -2,11 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Attributes\Guarded;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class Client extends Model
+#[Guarded(['id'])]
+class Client extends Model implements HasMedia
 {
     /** @use HasFactory<\Database\Factories\ClientFactory> */
     use HasFactory;
+    use InteractsWithMedia;
 }
